4b1bcfdd1c58bdba6592292c13549887aa170e90
Geronymo Fuhrmann <geronymo.fuhrmann@gmail.com>
2022-09-30 14:23:09 +0200

    WIP

--- main.py
+++ main.py
@@ -1,59 +1,90 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.metrics import dp
+
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from shops import Organisation
 
 Builder.load_file('design.kv')
+SHOP_LIST = ["ALBERT", "BILLA", "D-VÝROBCI", "KAUFLAND", "MAKRO", "PENNY", "TESCO", "DROBNÝ DÁRCE", "SBÍRKA POTRAVIN",
+             "PBC", "SBÍRKA MODLETICE", "NORMA", "JIP", "DM", "ROSSMANN", "LIDL", "COOP", "JINÉ"]
 
 
 class StartupScreen(Screen):
     def to_insert_screen(self):
-        self.manager.current = "insert_screen"
+        self.manager.current = "insert_org_screen"
 
     def to_output_screen(self):
-        self.manager.current = "output_screen"
+        self.manager.current = "org_output_screen"
 
 
-class InsertScreen(Screen):
+class InsertOrgScreen(Screen):
     def on_enter(self, *args):
-        db = Organisation("dchp")
+        db = Organisation('DCHP')
         for i in db.org_list():
-            btn = Button(text=str(i), size_hint_y=None, height=50)
+            btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_org_name)
             self.ids.box.add_widget(btn)
 
+    def get_org_name(self, obj):
+        print(obj.text)
+        global org_name
+        org_name = obj.text
+        self.manager.current = "insert_shop_screen"
 
-    # def confirm(self):
-    #     org = self.ids.org_name.text
-    #     shop = self.ids.shop_name.text
-    #     typ = self.ids.type.text
-    #     amount = self.ids.amount.text
-    #     organisation = Organisation(org)
-    #     organisation.insert(org, shop, typ, amount)
-    #     self.manager.current = "success_screen"
 
+class InsertShopScreen(Screen):
+    def on_enter(self, *args):
+        for i in SHOP_LIST:
+            btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_shop_name)
+            self.ids.box.add_widget(btn)
+
+    def get_shop_name(self, obj):
+        print(obj.text)
+        global shop_name
+        shop_name = obj.text
+        self.manager.current = "type_and_amount_screen"
+
+
+class TypeAndAmountScreen(Screen):
+    def get_type(self, input_type):
+        global type
+        if input_type == "a":
+            type = "a"
+        elif input_type == "b":
+            type = "b"
+        elif input_type == "c":
+            type = "c"
+        elif input_type == "m":
+            type = "c"
+
+    def confirm(self):
+        amount = self.ids.amount.text
+        organisation = Organisation(org_name)
+        organisation.insert(org_name, shop_name, type, amount)
+        self.manager.current = "success_screen"
+
+
+class OrgOutputScreen(Screen):
+    def on_enter(self, *args):
+        db = Organisation('DCHP')
+        for i in db.org_list():
+            btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_org_name)
+            self.ids.box.add_widget(btn)
+
+    def get_org_name(self, obj):
+        print(obj.text)
+        global output_org
+        output_org = obj.text
+        self.manager.current = "text_output_screen"
+
+#THIS DOESN'T WORK YET !!!
+class TextOutputScreen(Screen):
     def show_amount(self):
-        org = self.ids.org_name.text
-        shop = self.ids.shop_name.text
-        typ = self.ids.type.text
-        organisation = Organisation(org)
-        organisation.get_type_amount(org, shop, typ)
-
-
-class OutputScreen(Screen):
-    def show_amount(self):
-        org = self.ids.out_org_name.text
-        shop = self.ids.out_shop_name.text
-        typ = self.ids.out_type.text
-        organisation = Organisation(org)
-        amount = organisation.get_type_amount(org, shop, typ)
-        if amount[0]:
-            self.ids.out_org.text = f"Možství pro organizaci {org.capitalize()} " \
-                                    f"z obchodu {shop.capitalize()} typu {amount[0]}kg."
-        else:
-            self.ids.out_org.text = f"Nebyl nalezen žádný záznam."
-
+        org = Organisation(output_org)
+        amount = org.get_type_amount(output_org)
+        l = Label(text=f'[color=ff3333]{amount}[/color]', markup=True)
+        self.ids.box.add_widget(l)
     def return_to_menu(self):
         self.manager.transition.direction = "left"
         self.manager.current = "startup_screen"
--- shops.py
+++ shops.py
@@ -3,51 +3,27 @@ import sqlite3
 
 class Organisation:
     def __init__(self, name):
-        self.conn = sqlite3.connect("pb.db")
+        self.conn = sqlite3.connect("pbtest.db")
         self.cur = self.conn.cursor()
         self.cur.execute(f"CREATE TABLE IF NOT EXISTS {name}"
                          f" (id INTEGER PRIMARY KEY,"
-                         f"obchod TEXT, "
+                         f"obchod TEXT,"
                          f"typ_A TEXT,"
-                         f"typ_B TEXT,"
-                         f"typ_C TEXT,"
                          f"vaha INT,"
-                         f"datum TEXT)")
+                         f"datum TEXT);")
         self.conn.commit()
 
     def insert(self, org, shop, type, amount):
-        if type.upper() == "A":
-            self.cur.execute(
-                f'INSERT INTO {org}(obchod, typ_A, vaha, datum) VALUES(?, 1, ?, datetime("now", "localtime"))',
-                (shop, amount))
-        elif type.upper() == "B":
-            self.cur.execute(
-                f'INSERT INTO {org}(obchod, typ_B, vaha, datum) VALUES(?, 1, ?, datetime("now", "localtime"))',
-                (shop, amount))
-        elif type.upper() == "C":
-            self.cur.execute(
-                f'INSERT INTO {org}(obchod, typ_C, vaha, datum) VALUES(?, 1, ?, datetime("now", "localtime"))',
-                (shop, amount))
+        self.cur.execute(
+            f'INSERT INTO {org}(obchod, typ, vaha, datum) VALUES(?, ?, ?, datetime("now", "localtime"));',
+            (shop, type.upper(), amount))
         self.conn.commit()
 
-    def get_type_amount(self, org, shop, type):
-        if type.upper() == "A":
-            self.cur.execute(f"SELECT SUM(vaha) FROM {org} WHERE obchod='{shop}' AND typ_A=1")
-            amount = self.cur.fetchone()
-            self.conn.commit()
-            return amount
-
-        elif type.upper() == "B":
-            self.cur.execute(f"SELECT SUM(vaha) FROM {org} WHERE obchod='{shop}' AND typ_B=1")
-            amount = self.cur.fetchone()
-            self.conn.commit()
-            return amount
-
-        elif type.upper() == "C":
-            self.cur.execute(f"SELECT SUM(vaha) FROM {org} WHERE obchod='{shop}' AND typ_A=1")
-            amount = self.cur.fetchone()
-            self.conn.commit()
-            return amount
+    def get_type_amount(self, org):
+        self.cur.execute(f"SELECT obchod, typ, SUM(vaha) FROM {org} GROUP BY obchod, typ;")
+        amount = self.cur.fetchall()
+        self.conn.commit()
+        return amount
 
     def org_list(self):
         self.cur.execute("SELECT name FROM sqlite_master where type='table';")
