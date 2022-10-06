381644f035af43bba1908867f547248ef82dd161
Geronymo Fuhrmann <geronymo.fuhrmann@gmail.com>
2022-10-06 13:19:15 +0200

    WIP

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.metrics import dp
 
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.recycleview import RecycleView
 from kivy.uix.screenmanager import Screen, ScreenManager
 from shops import Organisation
 
@@ -20,8 +22,9 @@ class StartupScreen(Screen):
 
 
 class InsertOrgScreen(Screen):
-    def on_enter(self, *args):
+    def on_pre_enter(self, *args):
         db = Organisation('DCHP')
+
         for i in db.org_list():
             btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_org_name)
             self.ids.box.add_widget(btn)
@@ -31,6 +34,9 @@ class InsertOrgScreen(Screen):
         global org_name
         org_name = obj.text
         self.manager.current = "insert_shop_screen"
+
+    # def on_leave(self, *args):
+    #     self.ids.grid.clear_widgets()
 
 
 class InsertShopScreen(Screen):
@@ -47,16 +53,65 @@ class InsertShopScreen(Screen):
 
 
 class TypeAndAmountScreen(Screen):
+    # def on_enter(self, *args):
+    #     btn_a = Button(text="A - Sbírka potravin", ids='type_a', size_hint=(.5, .5), on_press=self.change_color)
+    #     btn_b = Button(text="B - Čerstvé potraviny", ids='type_b', size_hint=(.5, .5), on_press=self.change_color)
+    #     btn_c = Button(text="C - Trvanlivé potraviny a nápoje", ids='type_c', size_hint=(.5, .5),
+    #                    on_press=self.change_color)
+    #     btn_m = Button(text="M - Materialní a drogérie", ids='type_a', size_hint=(.5, .5), on_press=self.change_color)
+    #     btn_a.bind(on_press=lambda x: self.get_type('a'))
+    #     btn_b.bind(on_press=lambda x: self.get_type('b'))
+    #     btn_c.bind(on_press=lambda x: self.get_type('c'))
+    #     btn_m.bind(on_press=lambda x: self.get_type('m'))
+    #     self.ids.grid.add_widget(btn_a)
+    #     self.ids.grid.add_widget(btn_b)
+    #     self.ids.grid.add_widget(btn_c)
+    #     self.ids.grid.add_widget(btn_m)
+
     def get_type(self, input_type):
         global type
-        if input_type == "a":
-            type = "a"
-        elif input_type == "b":
-            type = "b"
-        elif input_type == "c":
-            type = "c"
-        elif input_type == "m":
-            type = "c"
+        type = input_type
+        self.change_color()
+
+    def change_color(self):
+        # 3 120 32 rgb for clicked button
+        # this part looks like crap, but I haven't figured out how to do it better...
+        if type == 'a':
+            self.ids.type_a.background_normal = ''
+            self.ids.type_b.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_c.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_m.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_a.background_color = (3 / 255, 120 / 255, 32 / 255, 1)
+            self.ids.type_b.background_color = (1, 1, 1, 1)
+            self.ids.type_c.background_color = (1, 1, 1, 1)
+            self.ids.type_m.background_color = (1, 1, 1, 1)
+        elif type == 'b':
+            self.ids.type_b.background_normal = ''
+            self.ids.type_a.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_c.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_m.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_b.background_color = (3 / 255, 120 / 255, 32 / 255, 1)
+            self.ids.type_a.background_color = (1, 1, 1, 1)
+            self.ids.type_c.background_color = (1, 1, 1, 1)
+            self.ids.type_m.background_color = (1, 1, 1, 1)
+        elif type == 'c':
+            self.ids.type_c.background_normal = ''
+            self.ids.type_a.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_b.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_m.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_c.background_color = (3 / 255, 120 / 255, 32 / 255, 1)
+            self.ids.type_a.background_color = (1, 1, 1, 1)
+            self.ids.type_b.background_color = (1, 1, 1, 1)
+            self.ids.type_m.background_color = (1, 1, 1, 1)
+        elif type == 'm':
+            self.ids.type_m.background_normal = ''
+            self.ids.type_a.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_b.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_c.background_normal = 'atlas://data/images/defaulttheme/button'
+            self.ids.type_m.background_color = (3 / 255, 120 / 255, 32 / 255, 1)
+            self.ids.type_a.background_color = (1, 1, 1, 1)
+            self.ids.type_b.background_color = (1, 1, 1, 1)
+            self.ids.type_c.background_color = (1, 1, 1, 1)
 
     def confirm(self):
         amount = self.ids.amount.text
@@ -84,7 +139,7 @@ class TextOutputScreen(Screen):
     def on_enter(self):
         org = Organisation(output_org)
         amount = org.get_type_amount(output_org)
-        self.ids.output.text = str(amount)
+        self.ids.content_text.text = str(amount)
 
     def return_to_menu(self):
         self.manager.transition.direction = "left"
--- shops.py
+++ shops.py
@@ -1,14 +1,14 @@
 import sqlite3
-
+import pandas as pd
 
 class Organisation:
     def __init__(self, name):
-        self.conn = sqlite3.connect("pbtest.db")
+        self.conn = sqlite3.connect("pd2.db")
         self.cur = self.conn.cursor()
         self.cur.execute(f"CREATE TABLE IF NOT EXISTS {name}"
                          f" (id INTEGER PRIMARY KEY,"
                          f"obchod TEXT,"
-                         f"typ_A TEXT,"
+                         f"typ TEXT,"
                          f"vaha INT,"
                          f"datum TEXT);")
         self.conn.commit()
@@ -20,9 +20,10 @@ class Organisation:
         self.conn.commit()
 
     def get_type_amount(self, org):
-        self.cur.execute(f"SELECT obchod, typ, SUM(vaha) FROM {org} GROUP BY obchod, typ;")
-        amount = self.cur.fetchall()
-        self.conn.commit()
+        # self.cur.execute(f"SELECT obchod, typ, SUM(vaha) FROM {org} GROUP BY obchod, typ;")
+        # amount = self.cur.fetchall()
+        # self.conn.commit()
+        amount = pd.read_sql_query(f'SELECT obchod, typ, SUM(vaha) FROM {org} GROUP BY obchod, typ;', self.conn)
         return amount
 
     def org_list(self):
@@ -30,4 +31,5 @@ class Organisation:
         tup_names = self.cur.fetchall()
         self.conn.commit()
         names = [i for tup in tup_names for i in tup]
+        names.remove("sqlite_sequence")
         return names
