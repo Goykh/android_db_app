d651b59e4c85591362a0dfc31d59de6b6cea5ed2
Geronymo Fuhrmann <geronymo.fuhrmann@gmail.com>
2022-10-31 15:32:25 +0100

    added comments and descriptions to functions

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ from kivy.uix.screenmanager import Screen, ScreenManager
 from shops import Organisation
 
 Builder.load_file('design.kv')
+# List of shops to generate buttons
 SHOP_LIST = ["ALBERT", "BILLA", "D-VÝROBCI", "KAUFLAND", "MAKRO", "PENNY", "TESCO", "DROBNÝ\nDÁRCE", "SBÍRKA\nPOTRAVIN",
              "PBC", "SBÍRKA\nMODLETICE", "NORMA", "JIP", "DM", "ROSSMANN", "LIDL", "COOP", "JINÉ"]
 
 
 class StartupScreen(Screen):
+    # The fist screen you enter when you open the app
+    # it has two buttons, one to add data and the other one to read data
     def to_insert_screen(self):
         self.manager.current = "insert_org_screen"
 
@@ -24,8 +27,17 @@ class StartupScreen(Screen):
 
 
 class InsertOrgScreen(Screen):
+    # Screen where you pick the organisation to add data
     def on_enter(self, *args):
+        """
+        Method to dynamically generate all the organisations as buttons.
+        the on_enter() makes it generate when you enter the screen.
+        """
+        # calling the db, I picked one org as default, I should change this in the future
         db = Organisation('DCHP')
+        # clearing the widgets
+        # mainly when you re-enter the screen so the buttons don't generate again
+        # on top of the old ones
         self.ids.box.clear_widgets()
         for i in db.org_list():
             btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_org_name, valign='center',
@@ -33,14 +45,22 @@ class InsertOrgScreen(Screen):
             self.ids.box.add_widget(btn)
 
     def get_org_name(self, obj):
-        print(obj.text)
+        """
+        Method to get the name of the organisation as a global variable.
+        :param obj: text on the button you pressed
+        """
         global org_name
         org_name = obj.text
         self.manager.current = "insert_shop_screen"
 
 
 class InsertShopScreen(Screen):
+    # same screen as before but with shops
     def on_enter(self, *args):
+        """
+        Method to dynamically generate all the shops as buttons from the SHOP_LIST constant.
+        the on_enter() makes it generate when you enter the screen.
+        """
         self.ids.box.clear_widgets()
         for i in SHOP_LIST:
             btn = Button(text=str(i), size_hint_y=None, height=50, on_release=self.get_shop_name, valign='center',
@@ -48,14 +68,22 @@ class InsertShopScreen(Screen):
             self.ids.box.add_widget(btn)
 
     def get_shop_name(self, obj):
+        """
+        Method to get the name of the shop as a global variable.
+        :param obj: text on the button you pressed
+        """
         global shop_name
         shop_name = obj.text
         self.manager.current = "type_and_amount_screen"
 
 
 class TypeAndAmountScreen(Screen):
-
+    # screen with 4 buttons to pick the food type
+    # and a calculator layout below it to add the amount
     def on_pre_enter(self, *args):
+        # THIS SHOULD RESET THE COLOURS OF THE BUTTONS
+        # WHEN YOU RE-ENTER THE SCREEN
+        # BUT IT DOES NOT WORK YET
         self.ids.calculator_input.text = ''
         self.ids.type_a.background_normal = 'atlas://data/images/defaulttheme/button'
         self.ids.type_b.background_normal = 'atlas://data/images/defaulttheme/button'
@@ -63,12 +91,20 @@ class TypeAndAmountScreen(Screen):
         self.ids.type_m.background_normal = 'atlas://data/images/defaulttheme/button'
 
     def get_type(self, input_type):
+        """
+        Method to get the type of food as a global variable.
+        :param obj: text on the button you pressed
+        """
         global food_type
         food_type = input_type
         self.change_color()
 
     def change_color(self):
-
+        """
+        Changes the colour of the clicked button to green
+        and back to grey when you click a different button.
+        I know that this is ugly AF and I need to rewrite this.
+        """
         # 3 120 32 rgb for clicked button
         # this part looks like crap, but I haven't figured out how to do it better...
         if food_type == 'a':
@@ -110,19 +146,37 @@ class TypeAndAmountScreen(Screen):
 
     # Calculator functions!
     def clear(self):
+        """
+        Method for when you press the C button on the calculator.
+        Clears the text input area and shows the hint_text.
+        """
         self.ids.calculator_input.text = ''
 
     # method to press buttons
-    def button_press(self, button):
+    def button_press(self, pressed_button):
+        """
+        Method gets called when a number button on the calculator is pressed.
+        Text is then added to the text input area.
+        :param pressed_button: value of the pressed button
+        """
         # variable that is the previous input in the input area
         previous_number = self.ids.calculator_input.text
         # checks if there is a number (it shouldn't be, but better to be safe)
         if previous_number in ['0', '+', '-', '*']:
-            self.ids.calculator_input.text = f'{button}'
+            # adds input to text area
+            self.ids.calculator_input.text = f'{pressed_button}'
         else:
-            self.ids.calculator_input.text += f'{button}'
+            # adds input to text area
+            self.ids.calculator_input.text += f'{pressed_button}'
 
     def math_operation(self, pressed_button):
+        """
+        Method when a math operation button is pressed (+, - or *).
+        Checks if the input area isn't already ending with one of the signs.
+        If yes, it replaces it.
+        :param pressed_button:  value of the pressed button
+        """
+        # previous input in the text input area
         previous_number = self.ids.calculator_input.text
         if previous_number.endswith('+') or previous_number.endswith('-') or previous_number.endswith('*'):
             self.ids.calculator_input.text = previous_number[:-1] + f'{pressed_button}'
@@ -130,6 +184,12 @@ class TypeAndAmountScreen(Screen):
             self.ids.calculator_input.text += f'{pressed_button}'
 
     def decimal_click(self):
+        """
+        Method for the decimal button.
+        Checks if it's already in the input text
+        or the text is ending with any other math operator.
+        If yes, it replaces it.
+        """
         previous_number = self.ids.calculator_input.text
         if previous_number.endswith('.'):
             pass
@@ -140,6 +200,12 @@ class TypeAndAmountScreen(Screen):
             self.ids.calculator_input.text += '.'
 
     def equals(self):
+        """
+        Gets called when the '=' button is pressed.
+        Uses the eval built in to calculate the result.
+        If the input text area is ending with a math operator (+, -, *)
+        It does the eval without it.
+        """
         previous_number = self.ids.calculator_input.text
         if previous_number.endswith('+') or previous_number.endswith('-') or previous_number.endswith('*'):
             evaluation = eval(previous_number[:-1])
@@ -148,10 +214,17 @@ class TypeAndAmountScreen(Screen):
         self.ids.calculator_input.text = str(evaluation)
 
     def confirm(self):
-        # this needs work on too
-
+        # this is not done yet
+        """
+        Gets called when the confirm button is pressed.
+        Creates global variable with the amount.
+        Looks for possible errors.
+        If error is found, it generates a popup window
+        to tell the user what was wrong.
+        """
         global input_amount
         input_amount = self.ids.calculator_input.text
+        # checks if input amount is empty
         if input_amount == '':
             popup = Popup(
                 title='Nastala chyba!',
@@ -160,7 +233,7 @@ class TypeAndAmountScreen(Screen):
                 pos_hint={'x': .2, 'top': .7},
                 content=Label(text='Zadání není správné! Můžete zadat jenom čísla'))
             popup.open()
-
+        # checks if any index in the input amount is a letter
         for i in input_amount:
             if i.isalpha():
                 popup = Popup(
@@ -171,7 +244,7 @@ class TypeAndAmountScreen(Screen):
                     content=Label(text='Zadání není správné! Můžete zadat jenom čísla.'))
                 popup.open()
 
-
+        # checks if food type has been selected, if not. Creates popup
         if food_type in ['a', 'b', 'c', 'm']:
             self.manager.current = "success_screen"
         else:
@@ -184,8 +257,68 @@ class TypeAndAmountScreen(Screen):
             popup.open()
 
 
-class OrgOutputScreen(Screen):
+class SuccessScreen(Screen):
+    # TODO: 2 new buttons:
+    # 1. button: go back to edit the input on the screen before if input was wrong
+    # 2. button: go back to InsertShopScreen to pick another shop for the same org
+    # make the sql query (.insert()) on this button or the return_to_menu button
+    #
+    # organisation = Organisation(org_name)
+    # organisation.insert(org_name, shop_name, type, input_amount)
+
+    # Confirmation screen
+    # You get a list of transactions made in the session.
+    # Generates 3 buttons to edit, add again or go back menu.
     def on_enter(self, *args):
+        """
+        On enter generates a label with the data you just added.
+        For now it stacks for every entry you did in the current session.
+        """
+        # declaring the self.label so I can access it in a function below
+        self.label = Label(
+            text=f'{org_name} - {shop_name} -  {food_type.upper()} - {input_amount}kg',
+            color=(0, 0, 0, 1))
+        self.ids.query_output.add_widget(self.label)
+
+    def return_to_calculator(self):
+        """
+        Returns to the previous screen if you see a mistake in the newest label.
+        Also deletes the newest label.
+        """
+        self.ids.query_output.remove_widget(self.label)
+        self.manager.transition.direction = "left"
+        self.manager.current = 'type_and_amount_screen'
+
+    def return_to_org_screen(self):
+        """
+        Returns to the screen where you pick a shop from the list
+        so you can add more data to one organisation.
+        Also makes the SQL query to add all the data to the database.
+        """
+        organisation = Organisation(org_name)
+        organisation.insert(org_name, shop_name, food_type, input_amount)
+        self.manager.transition.direction = "left"
+        self.manager.current = "insert_shop_screen"
+
+    def return_to_menu(self):
+        """
+        Returns to the startup_screen.
+        Also makes the SQL query to add all the data to the database.
+        """
+        organisation = Organisation(org_name)
+        organisation.insert(org_name, shop_name, food_type, input_amount)
+        self.manager.transition.direction = "left"
+        self.manager.current = "startup_screen"
+
+
+class OrgOutputScreen(Screen):
+    # Screen to pick orgs to the output screen
+    # basically a copy of the InsertOrgScreen
+    def on_enter(self, *args):
+        """
+        Method to dynamically generate all the organisations as buttons.
+        the on_enter() makes it generate when you enter the screen.
+        """
         self.ids.box.clear_widgets()
         db = Organisation('DCHP')
         for i in db.org_list():
@@ -194,13 +327,23 @@ class OrgOutputScreen(Screen):
             self.ids.box.add_widget(btn)
 
     def get_org_name(self, obj):
+        """
+        Method to get the name of the organisation as a global variable.
+        :param obj: text on the button you pressed
+        """
         global output_org
         output_org = obj.text
         self.manager.current = "text_output_screen"
 
 
 class TextOutputScreen(Screen):
-    def on_enter(self):
+    # Screen that outputs the data for a org.
+    def on_pre_enter(self):
+        """
+        On entering generates labels with the data from the .get_type_amount() method
+        from shops.py
+        """
+        # clears widgets on screen
         self.ids.output_grid.clear_widgets()
         org = Organisation(output_org)
         amount = org.get_type_amount(output_org)
@@ -217,48 +360,22 @@ class TextOutputScreen(Screen):
             # self.ids.output_grid.add_widget(line)
 
     def return_to_previous_screen(self):
+        """
+        Return to previous screen to see data from a different org
+        """
         self.manager.transition.direction = "left"
         self.manager.current = "org_output_screen"
 
     def return_to_menu(self):
+        """
+        Returns to main screen
+        """
         self.manager.transition.direction = "left"
         self.manager.current = "startup_screen"
 
 
 class RootWidget(ScreenManager):
     pass
-
-
-class SuccessScreen(Screen):
-    # TODO: 2 new buttons:
-    # 1. button: go back to edit the input on the screen before if input was wrong
-    # 2. button: go back to InsertShopScreen to pick another shop for the same org
-    # make the sql query (.insert()) on this button or the return_to_menu button
-    #
-    # organisation = Organisation(org_name)
-    # organisation.insert(org_name, shop_name, type, input_amount)
-
-    def on_enter(self, *args):
-        label = Label(
-            text=f'{org_name} - {shop_name} -  {food_type.upper()} - {input_amount}kg',
-            color=(0, 0, 0, 1))
-        self.ids.query_output.add_widget(label)
-
-    def return_to_calculator(self):
-        self.manager.transition.direction = "left"
-        self.manager.current = 'type_and_amount_screen'
-
-    def return_to_org_screen(self):
-        organisation = Organisation(org_name)
-        organisation.insert(org_name, shop_name, food_type, input_amount)
-        self.manager.transition.direction = "left"
-        self.manager.current = "insert_shop_screen"
-
-    def return_to_menu(self):
-        organisation = Organisation(org_name)
-        organisation.insert(org_name, shop_name, food_type, input_amount)
-        self.manager.transition.direction = "left"
-        self.manager.current = "startup_screen"
 
 
 class MainApp(App):
--- shops.py
+++ shops.py
@@ -1,8 +1,13 @@
 import sqlite3
 from xlsxwriter.workbook import Workbook
 
+
 class Organisation:
     def __init__(self, name):
+        """
+        Connects to the db, creates the table if it doesn't exist (it should).
+        :param name: name of db table
+        """
         self.conn = sqlite3.connect("pd2.db")
         self.cur = self.conn.cursor()
         self.cur.execute(f"CREATE TABLE IF NOT EXISTS {name}"
@@ -14,18 +19,38 @@ class Organisation:
         self.conn.commit()
 
     def insert(self, org, shop, type, amount):
+        """
+        Inserts data into a table.
+        Input comes from the Kivy buttons.
+        Also adds a date
+        :param org: name of the org -> table name
+        :param shop: name of the shop
+        :param type: type of the food (A, B, C, M)
+        :param amount: amount of food
+        """
         self.cur.execute(
             f'INSERT INTO {org}(obchod, typ, vaha, datum) VALUES(?, ?, ?, datetime("now", "localtime"));',
             (shop, type.upper(), amount))
         self.conn.commit()
 
     def get_type_amount(self, org):
+        """
+        Gets the amount of food by type from the DB
+        :param org: org name -> table name
+        :return: the amount grouped by shop name and type
+        """
+
         self.cur.execute(f"SELECT obchod, typ, SUM(vaha), datum FROM {org} GROUP BY obchod, typ;")
         amount = self.cur.fetchall()
         self.conn.commit()
         return amount
 
     def org_list(self):
+        """
+        This method is used to create the buttons.
+        Makes a list out of all table names.
+        :return: list with table names
+        """
         self.cur.execute("SELECT name FROM sqlite_master where type='table';")
         tup_names = self.cur.fetchall()
         self.conn.commit()
@@ -37,6 +62,12 @@ class Organisation:
 
     # TODO: try to come up with a solution to export the db to a csv file
     def to_csv_file(self, org):
+        """
+        CURRENTLY NOT USED, BUT WILL BE IN THE FUTURE!
+        Puts data from the SQL table into a csv file.
+        :param org: org name -> table name
+        :return: csv file with the data
+        """
         workbook = Workbook(f'{org}.xlsx')
         worksheet = workbook.add_worksheet()
         data = self.cur.execute(f'SELECT obchod, typ, SUM(vaha) FROM {org} GROUP BY obchod, typ;')
